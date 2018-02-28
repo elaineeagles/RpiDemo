bf2cfcef025540232a68e36d627750b1156eb54f
leeagles <eeagles@localhost>
2018-02-28 19:02:22 +0000

    fixing

--- sensors/add_data.php
+++ sensors/add_data.php
@@ -3,14 +3,16 @@
     include("connect.php");
     $snum = $_GET['snum'];
     $sval = $_GET['sval'];
+    //$snum = 1;
+    //$sval = 5;
 
      date_default_timezone_set('Europe/London');
     $dateS = date("Y-m-d h:i:sa", time());
     //echo $dateS;
 	
 	// Prepare the SQL statement
-    $SQL = "INSERT INTO myDB.sensordata (snum, sval, date) VALUES ($snum, $sval, '$dateS')";     
-
+    $SQL = "INSERT INTO myDB.sensordata (snum, sval) VALUES ($snum, $sval)";     
+    echo $SQL;
     // Execute SQL statement
     mysql_query($SQL);
 
