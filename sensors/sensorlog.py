d991d977e8075a7e5c4443e2fb5deff26ec93c92
leeagles <eeagles@localhost>
2018-02-28 19:23:47 +0000

    fixing

--- sensors/sensorlog.py
+++ sensors/sensorlog.py
@@ -1,23 +1,24 @@
 import os
-import threading
+from threading import Thread
 import urllib2
 import random
+import time
 
 #add sensor reading code here
 def readSensor():
-	global sval 
-	sval = random.randint(0,5)
-	
-# function to send data read from sensor to WebAPI
-def sendDataToServer():
-	global sval
-
-	threading.Timer(600,sendDataToServer).start()
+        sval = 0
 	print("Sensing...")
-	readSensor()
+	sval = random.randint(1,5)
+        #time.sleep(5)
 	print(sval)
 	myurl = "http://localhost/RPi/sensors/add_data.php?snum=1&sval="+str(sval)
 	urllib2.urlopen(myurl).read()
+	
+# function to send data read from sensor to WebAPI
+def sendDataToServer():
+        for i in range (10):
+	 	t = Thread(target=readSensor, args=())	
+                t.start()
 
 #main part of program
 sendDataToServer()
