b58785c61dc7625e6fc46b7f559f521d86f3d339
Lili Elaine Eagles <leeagles@users.noreply.github.com>
2018-02-28 17:58:19 +0000

    fixed

--- sensors/DataLogger.py
+++ sensors/DataLogger.py
@@ -7,8 +7,7 @@ import random
 def readSensor():
 	global sval 
 	sval = random.randint(0,5)
-	sval = round(sval,1)
-
+	
 # function to send data read from sensor to WebAPI
 def sendDataToServer():
 	global sval
@@ -16,7 +15,7 @@ def sendDataToServer():
 	threading.Timer(600,sendDataToServer).start()
 	print("Sensing...")
 	readSensor()
-	print("sensor " + sval)
+	print("sensor ",sval)
 	myurl = "http://localhost/RPi/sensors/add_data.php?snum=1&sval="+sval
 	urllib2.urlopen(myurl).read()
 
