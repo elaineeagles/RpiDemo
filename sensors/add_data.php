94432748e9cf682f774aac5a979729eeba9096c9
Lili Elaine Eagles <leeagles@users.noreply.github.com>
2018-02-28 17:43:49 +0000

    Updated

--- sensors/add_data.php
+++ sensors/add_data.php
@@ -1,16 +1,19 @@
 <?php
     // Connect to MySQL
     include("connect.php");
+    $snum = $_GET['snum'];
+    $sval = $_GET['sval'];
 
-    // Prepare the SQL statement
-      date_default_timezone_set('Europe/London');
-     $dateS = date("Y-m-d h:i:sa", time());
+     date_default_timezone_set('Europe/London');
+    $dateS = date("Y-m-d h:i:sa", time());
     //echo $dateS;
-    $SQL = "INSERT INTO YOUR-DATABASE-NAME.data (date,temperature,humidity,pressure) VALUES ('$dateS','".$_GET["temp"]."','".$_GET["hum"]."','".$_GET["pr"]."')";     
+	
+	// Prepare the SQL statement
+    $SQL = "INSERT INTO myDB.sensordata (snum, sval, date) VALUES ($snum, $sval, '$dateS')";     
 
     // Execute SQL statement
     mysql_query($SQL);
 
     // Go to the review_data.php (optional)
-    header("Location:http://www.it-computer.co.uk/index.php");
+    header("Location:index.php");
 ?>
